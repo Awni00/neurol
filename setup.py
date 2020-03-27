9315419fb1b150d08a0b3c7b3c7e8de837491085
Awni00 <awnyaltabaa@gmail.com>
2020-03-27 00:47:53 -0400

    add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="neurol",
+    version="0.0.1",
+    author="Awni",
+    author_email="awni.altabaa@queensu.ca",
+    description="A package for modularly implenting Brain-Computer Interfaces",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="BCI Brain-Computer Interface Neurotechnology Neuroscience EEG",
+    install_requires=[
+            "numpy",
+            "matplotlib",
+            "biosppy",
+            "ble2lsl",
+            "pylsl",
+            "scipy",
+            "scikit_learn",
+            "Keras"
+    ],
+    include_package_data=True,
+    # url="https://github.com/...",
+    #project_urls={'Documentation': '', 'Source':'', 'Tracker':''},
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
