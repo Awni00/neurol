c532a9c9cd0b66925f1621a586b7fb548f2bae34
Awni00 <awnyaltabaa@gmail.com>
2020-03-29 02:13:36 -0400

    feat: get_EEG_inlets

--- neurol/connect_device.py
+++ neurol/connect_device.py
@@ -6,6 +6,8 @@ devices.
 import ble2lsl
 from ble2lsl.devices import muse2016
 
+from pylsl import StreamInlet, resolve_byprop
+
 
 def connect_muse():
     '''
@@ -13,3 +15,16 @@ def connect_muse():
     returns ble2lsl.ble2lsl.Streamer object.
     '''
     return ble2lsl.Streamer(muse2016)
+
+
+def get_EEG_inlets():
+    '''resolves all EEG streams and returns their inlets in an array.'''
+
+    stream_infos = resolve_byprop("type", "EEG")
+
+    inlets = []
+
+    for stream_info in stream_infos:
+        inlets.append(StreamInlet(stream_info, ))
+
+    return inlets
