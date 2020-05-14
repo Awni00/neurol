596b0b8ee07aca97b2615294eacbd023bf1dff55
Awni Al Tabaa <awnyaltabaa@gmail.com>
2020-05-14 19:46:14 -0400

    implement retentive_BCI

--- neurol/BCI.py
+++ neurol/BCI.py
@@ -27,9 +27,10 @@ class generic_BCI:
         calibrator: a function which is run on startup to perform
             calibration using `inlet`; returns `calibration_info`
             which is used by `classifier` and `transformer`.
+        calibration_info: the result of the calibrator, if applicable.
         buffer_length(int): the length of the `buffer`; specifies the
             number of samples of the signal to keep for classification.
-
+        brain_state: the most recent brain state classification.
 
     '''
 
@@ -208,8 +209,10 @@ class fsm_BCI(generic_BCI):
         calibrator: a function which is run on startup to perform
             calibration using `inlet`; returns `calibration_info`
             which is used by `classifier` and `transformer`.
+        calibration_info: the result of the calibrator, if applicable.
         buffer_length(int): the length of the `buffer`; specifies the
             number of samples of the signal to keep for classification.
+        brain_state: the most recent brain state classification.
 
     '''
 
@@ -237,8 +240,108 @@ class fsm_BCI(generic_BCI):
                                                    self.calibration_info)
             except TypeError as type_err:
                 print(('Got TypeError when calling classifier. \n\n'
-                       'Make sure your classifier is a function which '
-                       'accepts (clf_input, calibration_info) as inputs.'))
+                       'Make sure your classifier is a function which accepts '
+                       '(clf_input, brain_state, calibration_info) as inputs.'))
+                raise type_err
+
+            try:
+                # run action based on classification
+                self.action(self.brain_state)
+            except TypeError as type_err:
+                print(('Got TypeError when calling action. \n\n'
+                       'Make sure your action is a function which '
+                       'accepts (brain_state) as an input.'))
+                raise type_err
+
+
+class retentive_BCI(generic_BCI):
+    '''
+    Implements a Brain-Computer Interface with memory of past brain states.
+
+    Classification of brain-state is not only dependent on the transformed
+    real-time brain signal, but also the finite list of previous brain states.
+
+    Internally manages a buffer of the signal given in `inlet` and
+    continuously performs classification on appropriately transformed
+    real-time neural data. At each classification, a corresponding `action`
+    is performed.
+
+    Attributes:
+        classifier: a function which performs classification on the
+            most recent data (transformed as needed). returns classification.
+        transformer: function which takes in the most recent data (`buffer`)
+            and returns the transformed input the classifer expects.
+        action: a function which takes in the classification, and
+            performs some action.
+        calibrator: a function which is run on startup to perform
+            calibration using `inlet`; returns `calibration_info`
+            which is used by `classifier` and `transformer`.
+        calibration_info: the result of the calibrator, if applicable.
+        buffer_length(int): the length of the `buffer`; specifies the
+            number of samples of the signal to keep for classification.
+        brain_state: the most recent brain state classification.
+        memory_length(int): number of brain states into the past to remember.
+        past_states: a list of the past classifications of brain states.
+            used in next classification. length is memory_length.
+    '''
+
+    def __init__(self, classifier, transformer=None, action=print,
+                 calibrator=None, buffer_length=1024, memory_length=10):
+        '''
+        Initialize a generic BCI object.
+
+        See class documentation for infromation about the class itself.
+
+        Arguments:
+            classifier: a function which performs classification on the
+                most recent data (transformed as needed). returns class.
+            transformer: function which takes in the most recent data (`buffer`)
+                and returns the transformed input the classifer expects.
+            action: a function which takes in the classification, and
+                performs some action.
+            calibrator: a function which is run on startup to perform
+                calibration using `inlet`; returns `calibration_info`
+                which is used by `classifier` and `transformer`.
+            buffer_length(int): the length of the `buffer`; specifies the
+                number of samples of the signal to keep for classification.
+            memory_length(int): number of brain states to remember into past.
+        '''
+
+        super().__init__(classifier,
+                         transformer, action, calibrator, buffer_length)
+
+        self.memory_length = memory_length
+        self.past_states = []
+
+    def _update(self, buffer):
+        if buffer.shape[0] > self.buffer_length:
+            # clip to buffer_length
+            buffer = buffer[-self.buffer_length:]
+
+            # transform buffer for classification
+            if self.transformer is not None:
+                try:
+                    clf_input = self.transformer(buffer, self.calibration_info)
+                except TypeError as type_err:
+                    print(('Got TypeError when calling transformer.\n\n'
+                           'Make sure your transformer is a function '
+                           'which accepts (buffer, calibration_info) as inputs.'
+                           ))
+                    raise type_err
+            else:
+                clf_input = buffer
+
+            try:
+                # perform classification
+                self.brain_state = self.classifier(clf_input, self.past_states,
+                                                   self.calibration_info)
+                self.past_states.append(self.brain_state)
+                if len(self.past_states) > self.memory_length:
+                    self.past_states = self.past_states[-self.memory_length:]
+            except TypeError as type_err:
+                print(('Got TypeError when calling classifier. \n\n'
+                       'Make sure your classifier is a function which accepts '
+                       '(clf_input, past_states, calibration_info) as inputs.'))
                 raise type_err
 
             try:
