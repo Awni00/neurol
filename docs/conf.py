9d9e263162cc37bdd2acafa7126a2999da0e0ced
Awni Al Tabaa <awnyaltabaa@gmail.com>
2020-10-31 15:11:57 -0400

    docs: tweaked instance variables of classes

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,7 @@ autodoc_default_options = {
     'undoc-members': True,
     'exclude-members': '__weakref__'
 }
+napoleon_use_ivar = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
